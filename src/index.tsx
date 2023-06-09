453069b6ea8270a34d686b750759fcd8d4feed28
Zhenya Luzhanski <luzhanskizhenya@gmail.com>
2023-06-09 14:52:23 +0300

    Commit test method map

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {splitIntoWords} from './unit-test/01/01';
+
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
